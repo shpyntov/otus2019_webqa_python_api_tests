894f0843d36b9640eee4182e76ee66bb9d7ca439
Yury Shpyntov <shpyntov.y@ya.ru>
2020-01-25 11:36:49 +0300

    dogs api tests

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+import random
+
+
+class APIClient:
+    def __init__(self, base_address):
+        self.base_address = base_address
+
+    def post(self, path="/", params=None, data=None, headers=None):
+        url = self.base_address + path
+        return requests.post(url=url, params=params, data=data, headers=headers)
+
+    def get(self, path="/", params=None):
+        url = self.base_address + path
+        return requests.get(url=url, params=params)
+
+
+@pytest.fixture()
+def dog_api_client():
+    base_url = 'https://dog.ceo/api'
+    return APIClient(base_url)
+
+
+@pytest.fixture()
+def random_breed(dog_api_client):
+    breeds = dog_api_client.get(path='/breeds/list/all').json()['message']
+    return random.choice(list(breeds))
---
+++ tests/test_dog_api.py
@@ -0,0 +1,49 @@
+import pytest
+
+
+def test_random_image(dog_api_client):
+    result = dog_api_client.get(
+        path='/breeds/image/random'
+    )
+    assert result.status_code == 200
+    assert result.json()['status'] == "success"
+    assert result.json()['message'][-3:] == 'jpg'
+
+
+def test_random_breed_image(dog_api_client, random_breed):
+    result = dog_api_client.get(
+        path='/breed/' + random_breed + '/images/random'
+    )
+    assert result.status_code == 200
+    assert result.json()['status'] == "success"
+    assert result.json()['message'][-3:] == 'jpg'
+
+
+@pytest.mark.parametrize('images_count', [3, 5, 7])
+def test_couple_images_by_breed(dog_api_client, random_breed, images_count):
+    result = dog_api_client.get(
+        path='/breed/' + random_breed + '/images/random/' + str(images_count)
+    )
+    assert result.status_code == 200
+    assert result.json()['status'] == "success"
+    assert len(result.json()['message']) == images_count
+
+
+@pytest.mark.parametrize('breed', ["hound", "poodle", "retriever"])
+def test_subbreed_not_null(dog_api_client, breed):
+    result = dog_api_client.get(
+        path='/breed/' + breed + '/list'
+    )
+    assert result.status_code == 200
+    assert result.json()['status'] == "success"
+    assert len(result.json()['message']) != 0
+
+
+@pytest.mark.parametrize('breed, subbreed', [("retriever", "golden"), ("spaniel", "cocker"), ("terrier", "fox")])
+def test_random_subbredd_image(dog_api_client, breed, subbreed):
+    result = dog_api_client.get(
+        path='/breed/' + breed + '/' + subbreed + '/images/random'
+    )
+    assert result.status_code == 200
+    assert result.json()['status'] == "success"
+    assert result.json()['message'][-3:] == 'jpg'
